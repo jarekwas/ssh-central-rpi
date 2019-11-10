7fe7968a4401b9b0534207d532c9c81e7f94b5fa
Jarek Wasowski <jarek@truesoftware.pl>
2019-11-10 17:27:12 +0100

    x

--- ci/depolyArduinoAndDockers.sh
+++ ci/depolyArduinoAndDockers.sh
@@ -3,6 +3,7 @@
 docker-compose -v
 docker -v
 docker-compose -p domoticz down
+echo ${ENV_DEPLOY_MYSENSORS_GATEWAY_REPO_URL}
 git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@${ENV_DEPLOY_MYSENSORS_GATEWAY_REPO_URL} app
 cd app
 git checkout $APP_COMMIT_SHA
