df6803d938e3cc1269947b5f9617211bf8c77096
Jarek Wasowski <jarek@truesoftware.pl>
2019-11-13 19:09:51 +0100

    Podmiana na skrypt sh kompilujacy appke

--- ci/depolyArduinoAndDockers.sh
+++ ci/depolyArduinoAndDockers.sh
@@ -8,9 +8,7 @@ git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@${ENV_DEPLOY_MYSENSORS_GATEWAY
 cd app
 git checkout $APP_COMMIT_SHA
 git submodule update --init
-arduino-cli lib install "MySensors"
-arduino-cli lib install "Bounce2"
-arduino-cli compile --fqbn arduino:avr:mega App
+sh ci/compile.sh
 arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega App
 cd ..
 docker-compose -p domoticz up -d
